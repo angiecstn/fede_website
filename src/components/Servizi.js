05a51316038fe950fe301c9998c2c9ebdc412328
Angelica Costantini <angelicacostantini@DWMAC-C02YW23ALVDL.lan>
2020-05-27 20:01:02 +0200

    smaller icons, fixing typos

--- src/components/Servizi.js
+++ src/components/Servizi.js
@@ -10,31 +10,31 @@ class Servizi extends React.Component {
                     <div className="ui big relaxed horizontal list servizi">
                     <h2 className="title">Servizi Offerti:</h2>
                     <div className="item">
-                        <img className="ui avatar image" alt="dog icon" src="images/dog.png" />
+                        <img className="ui avatar image" alt="dog icon" src="images/dog.png" style={{'font-size':12}}/>
                         <div className="content">
-                            Toalettatura Cani
+                            Toelettatura Cani
                         </div>
                     </div>
                     <div className="item">
-                        <img className="ui avatar image" alt="cat icon" src="images/cat.png" />
+                        <img className="ui avatar image" alt="cat icon" src="images/cat.png" style={{'font-size':12}} />
                         <div className="content">
-                            Toalettatura Gatti
+                            Toelettatura Gatti
                         </div>
                     </div>
                     <div className="item">
-                        <img className="ui avatar image" alt="shampoo bottle icon" src="images/shampoo.png" />
+                        <img className="ui avatar image" alt="shampoo bottle icon" src="images/shampoo.png"  style={{'font-size':12}}/>
                         <div className="content">
-                            Prodotti per Toalettatura
+                            Prodotti per Toelettatura
                         </div>
                     </div>
                     <div className="item">
-                        <img className="ui avatar image" alt="scissor icon" src="images/scissors.png" />
+                        <img className="ui avatar image" alt="scissor icon" src="images/scissors.png" style={{'font-size':12}} />
                         <div className="content">
                             Stripping
                         </div>
                     </div>
                     <div className="item">
-                        <img className="ui avatar image" alt="treatment icon" src="images/treatment.png" />
+                        <img className="ui avatar image" alt="treatment icon" src="images/treatment.png" style={{'font-size':12}} />
                         <div className="content">
                             Ozonoterapia
                         </div>
