23cce5dd736a17c228339a188ef7ac25efcf800e
Angelica Costantini <angelicacostantini@DWMAC-C02YW23ALVDL.lan>
2020-05-27 18:40:32 +0200

    fixing scroll on elements

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -38,8 +38,8 @@ export class Navbar extends Component {
                     <img src="/images/default.png" onClick={this.scrollToTop} alt="Federigo logo" height="100" width="100" />
                     {/* <a className="active item" href="home"onClick={this.scrollToTop}>Home</a> */}
                     <div className="right small yellow menu">
-                    <div className="item"><Link activeClass="active" className="about" to="about1" spy={true} smooth={true} duration={500} >About</Link></div>
-                    <div className="item"><Link activeClass="active" className="servizi" to="servizi1" spy={true} smooth={true} duration={500} >Servizi</Link></div>
+                    <div className="item"><Link activeClass="active" className="about" to="about1" offset={-120} spy={true} smooth={true} duration={500} >About</Link></div>
+                    <div className="item"><Link activeClass="active" className="servizi" to="servizi1" offset={-120} spy={true} smooth={true} duration={500} >Servizi</Link></div>
                         <div className="item"><Link activeClass="active" className="contatti" to="contatti1" spy={true} smooth={true} duration={500} >Contatti</Link></div>
                     </div>
                 </div>
